61315d517810e9d7bbb292d91a7d06c6c268ccd0
Felipe Ferreira <felipesqf@gmail.com>
2021-01-22 00:25:33 +1100

    add images

--- empdir/src/utils/API.js
+++ empdir/src/utils/API.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 //get users from the randomusers api
+// eslint-disable-next-line import/no-anonymous-default-export
 export default {
   getEmployees: function() {
     return axios.get('https://randomuser.me/api/?results=100', {
